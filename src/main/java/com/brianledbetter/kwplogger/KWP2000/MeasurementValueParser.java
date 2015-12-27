fe87017e0f0906a087c07d1ae3f3f8bb1b5b3b40
Bri3D <brian@brianledbetter.com>
2015-12-26 18:47:34 -0700

    move KWP2000 code into library

---
+++ src/main/java/com/brianledbetter/kwplogger/KWP2000/MeasurementValueParser.java
@@ -0,0 +1,8 @@
+package com.brianledbetter.kwplogger.KWP2000;
+
+/**
+ * Created by b3d on 12/6/15.
+ */
+public interface MeasurementValueParser {
+    MeasurementValue parseBytes(int[] bytes); // we use int because of wonder, wonderful signed bytes
+}
